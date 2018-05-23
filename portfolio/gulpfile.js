6543f6b588f23efae729726b03981eeb6e712190
LindaKadz <achielinda44@gmail.com>
2018-05-23 11:10:56 +0300

    Learning of gulp copyJS

---
+++ portfolio/gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require ('gulp');
+
+const sass = require ('gulp-sass');
+
+gulp.task('copyHTML', function () {
+  return gulp.src('*.html')
+     .pipe(gulp.dest('portfolio/html'))
+})
+
+gulp.task('copyCSS', function () {
+  return gulp.src('*.scss')
+    .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('portfolio'))
+})
+
+gulp.task('copyJavaScript', function () {
+  return gulp.src('*.js')
+  .pipe(gulp.dest('portfolio'))
+})
---
+++ portfolio/script.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    document.getElementById("title").innerHTML = 'Hey! Velmah here!';
+}
+
+var myElement = document.getElementById("text");
+document.getElementById("button").innerHTML =
+"The text from the intro paragraph is " + myElement.innerHTML;
